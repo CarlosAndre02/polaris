4a1bc1d4ac97c10d73d37e572b3341a84ed7d2f7
Carlos André <cagd2002@gmail.com>
2023-05-16 11:06:48 -0300

    add language model and routes

---
+++ src/modules/language/LanguageRoutes.ts
@@ -0,0 +1,18 @@
+import express from 'express'
+const router = express.Router()
+
+import { LanguageModel } from './LanguageModel'
+
+router.get("/language", async function (req, res) {
+    try {
+        const languages = await LanguageModel.find()
+        res.status(200).json({ languages });
+    } catch(e) {
+        res.status(400).json({
+            errorTitle: "Um erro aconteceu na hora de buscar.",
+            errorMessage: e,
+        });
+    }
+})
+
+export default router
--- src/server.ts
+++ src/server.ts
@@ -2,15 +2,19 @@ import dotenv from 'dotenv';
 import express from 'express';
 dotenv.config();
 
+import languagesRoutes from './modules/language/LanguageRoutes'
+import lpUserRoutes from './modules/lp_user/LP_UserRoutes'
+import { seedDB } from './config/seedDB'
+
+seedDB()
+
 const app = express()
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 // Routes
-// app.use(require('./routes/user'));
-app.get('/', (req, res) => {
-    res.send('Hello World!')
-})
+app.use(languagesRoutes);
+app.use(lpUserRoutes);
 
 export default app
