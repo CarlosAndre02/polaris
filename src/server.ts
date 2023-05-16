327d3ae8b9deb8a40829117249393dd4a0c05a9b
Carlos André <cagd2002@gmail.com>
2023-05-16 19:14:51 -0300

    configure cors

--- src/server.ts
+++ src/server.ts
@@ -1,20 +1,35 @@
 import dotenv from 'dotenv';
 import express from 'express';
+import cors from 'cors';
 dotenv.config();
 
-import languagesRoutes from './modules/language/LanguageRoutes'
-import lpUserRoutes from './modules/lp_user/LP_UserRoutes'
-import { seedDB } from './config/seedDB'
+import languagesRoutes from './modules/language/LanguageRoutes';
+import lpUserRoutes from './modules/lp_user/LP_UserRoutes';
+import { seedDB } from './config/seedDB';
 
-seedDB()
+seedDB();
 
-const app = express()
+const app = express();
 
+app.use(cors());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-// Routes
-app.use(languagesRoutes);
-app.use(lpUserRoutes);
+app.use(function (req, res, next) {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader(
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content-Type, Accept'
+  );
+  res.setHeader(
+    'Access-Control-Allow-Methods',
+    'POST, GET, PATCH, DELETE, OPTIONS'
+  );
+  next();
+});
 
-export default app
+// Routes
+app.use('/api', languagesRoutes);
+app.use('/api', lpUserRoutes);
+
+export default app;
