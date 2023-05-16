55f93f9f997d54813eb4cfe838b20391e95fe015
Carlos André <cagd2002@gmail.com>
2023-05-16 19:14:16 -0300

    add imageUrl field in language model

--- src/modules/language/LanguageModel.ts
+++ src/modules/language/LanguageModel.ts
@@ -1,40 +1,46 @@
 import mongoose, { Schema, Document } from 'mongoose';
 
 export type ILanguage = {
-    name: string
-    duration: number
-    teacher: string
-    description: string
-    content: string
+  name: string;
+  duration: number;
+  teacher: string;
+  imageUrl: string;
+  description: string;
+  content: string;
 };
 
 export type LanguageDocument = Document & ILanguage;
 
-const LanguageSchema = new Schema(
-  {
-    name: {
-      type: String,
-      required: true,
-    },
-    duration: {
-      type: Number,
-      required: true,
-    },
-    teacher: {
-      type: String,
-      required: true,
-    },
-    description: {
-      type: String,
-      required: true,
-      minLength: 3
-    },
-    content: {
-      type: String,
-      required: true,
-      minLength: 3
-    },
+const LanguageSchema = new Schema({
+  name: {
+    type: String,
+    required: true
+  },
+  duration: {
+    type: Number,
+    required: true
+  },
+  teacher: {
+    type: String,
+    required: true
+  },
+  imageUrl: {
+    type: String,
+    required: true
+  },
+  description: {
+    type: String,
+    required: true,
+    minLength: 3
+  },
+  content: {
+    type: String,
+    required: true,
+    minLength: 3
   }
-);
+});
 
-export const LanguageModel = mongoose.model<LanguageDocument>('Language', LanguageSchema);
+export const LanguageModel = mongoose.model<LanguageDocument>(
+  'Language',
+  LanguageSchema
+);
